51eab9e3a2ec4a3956049d180dfd6db28a2f0c7c
BeBaal <86570412+BeBaal@users.noreply.github.com>
2022-07-23 12:59:57 +0200

    Initial Upload

---
+++ Skripte/GetFormData.js
@@ -0,0 +1,22 @@
+/* 
+    Diese Java Skript ließt die Daten aus dem HTML Form aus und überträgt die Daten zur Datenbank.
+*/
+
+
+document
+    .getElementById("myBtn2")
+    .addEventListener("click", dataPush);
+
+
+function dataPush() {
+    let l_firstname = document.getElementById("firstname")
+    let l_lastname = document.getElementById("lastname")
+    let l_mail = document.getElementById("mail")
+    let l_tel = document.getElementById("tel")
+    let l_b_date = document.getElementById("b_date")
+    let l_message = document.getElementById("message")
+    let l_confirmation = document.getElementById("confirmation")
+
+    console.log(l_firstname.value)
+
+}
