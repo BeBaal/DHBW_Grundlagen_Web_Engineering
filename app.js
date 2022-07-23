309f105861b257f35169106261f5efd3e024620b
BeBaal <86570412+BeBaal@users.noreply.github.com>
2022-07-23 17:06:31 +0200

    fix

--- app.js
+++ app.js
@@ -9,8 +9,13 @@ const server = http.createServer((req, res) => {
     res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, hostname, (error) => {
+
+    if (error) {
+        console.log('Server ist nicht gestartet.', error)
+    } else {
+        console.log(`Server running at http://${hostname}:${port}/`);
+    }
 });
 
 
